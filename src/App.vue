d181f37b0127bd4bc495771105c6820e11e07715
stepan.rutz <stepan.rutz@gmx.de>
2026-01-31 20:15:19 +0100

    view

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@ import { useQueryClient } from "@tanstack/vue-query";
 import { fetchProduct, useProduct } from "./composables/useProduct";
 import "./index.css";
 import { ref } from "vue";
+import ProductView from "./components/ProductView.vue";
 
 const client = useQueryClient();
-const PREFETCH = true;
+const PREFETCH = !true;
 if (PREFETCH) {
   (async () => {
     for (let i = 1; i <= 10; i++) {
@@ -30,16 +31,21 @@ function delay(ms: number) {
 
 const id = ref(1)
 const query = useProduct(id);
+const { data: product } = query
 
 </script>
 
 <template>
   <div class="bg-white w-screen h-screen flex flex-col items-center justify-center">
-    <button class="mb-4 px-4 py-2 bg-blue-500 text-white rounded" @click="id++">
-      Next Product
-    </button>
-    <pre class="bg-gray-100 p-2 border border-gray-300 text-sm">{{ query.data.value?.title }}
-    </pre>
+    <div class="flex gap-4">
+      <button class="mb-4 px-4 py-2 bg-blue-500 text-white rounded" @click="id--">
+        Prev Product
+      </button>
+      <button class="mb-4 px-4 py-2 bg-blue-500 text-white rounded" @click="id++">
+        Next Product
+      </button>
+    </div>
+    <ProductView v-if="product" :product="product" />
   </div>
 </template>
 
---
+++ src/components/ProductView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-[400px] h-[600px] p-4 rounded-lg shadow-lg bg-white border border-gray-200 flex flex-col gap-2">
+    <h2 class="text-lg font-bold">{{ product.title }}</h2>
+    <img class="h-1 grow w-full object-cover rounded-md" :src="product.image" :alt="product.title" />
+    <div class="self-stretch text-sm text-muted-300 flex justify-between">
+      <div>
+        Category: {{ product.category }} {{ product.id }}
+      </div>
+      <div>
+        Price: {{ formatPrice(product.price) }}
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import type { Product } from '@/composables/useProduct';
+
+const nf = new Intl.NumberFormat("de-DE", { maximumFractionDigits: 2, currency: "EUR", style: "currency" });
+
+function formatPrice(value: number) {
+  return nf.format(value);
+}
+
+const { product } = defineProps<{
+  product: Product
+}>()
+
+</script>
