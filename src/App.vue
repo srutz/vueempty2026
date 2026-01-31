6d93abce1a867e2b0a970398c1e6a653c97a16c9
stepan.rutz <stepan.rutz@gmx.de>
2026-01-31 17:31:17 +0100

    drop1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
-import Header from './components/Header.vue';
-import { FHeader } from './components/FHeader';
 import "./index.css";
 
 
 </script>
 
 <template>
-  <div class="bg-yellow-700 w-screen h-screen flex flex-col items-center justify-center p-4">
-    <div class="self-stretch h-1 grow bg-red-500">
-      <Header>My Vue 3 App</Header>
-      <FHeader title="My Header">My Vue 3 App FHeader</FHeader>
-    </div>
+  <div class="bg-white w-screen h-screen flex flex-col items-center justify-center">
+    <div>Hello World</div>
   </div>
 </template>
 
