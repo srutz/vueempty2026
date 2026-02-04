aff20a1c71b2dd0307ff2c6461ed3b46a8beddad
stepan.rutz <stepan.rutz@gmx.de>
2026-02-04 14:42:37 +0100

    added basic routes and menu

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,24 @@
 import App from '@/App.vue'
-import Main from '@/Main.vue'
-import About from '@/About.vue'
+import Frame from '@/Frame.vue'
+import NotFound from '@/NotFound.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/", component: Main
-    },
-    {
-      path: "/product/:id", component: App 
-    },
-    {
-      path: "/about", component: () => import("@/About.vue")
+      path: "/", component: Frame,
+      children: [
+        {
+          path: "/product/:id", component: App,
+        },
+        {
+          path: "/about", component: () => import("@/About.vue")
+        },
+        {
+          path: '/:pathMatch(.*)*', component: NotFound
+        }
+      ]
     },
   ],
 })
